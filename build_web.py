cd5d7b7de80de92a4d806fc2f9792abb7741351e
Anthony Metzidis <anthony.metzidis@gmail.com>
2025-01-22 11:36:01 -0800

    clean data dir

--- build_web.py
+++ build_web.py
@@ -4,16 +4,22 @@ from shutil import copytree, rmtree, copy2
 from os.path import join, isdir, basename, splitext
 from sys import platform
 import hashlib
+import shutil
 Import("env")
 
-if env.IsCleanTarget():
-    print("clean: no web build")
-    Return()
+clean_dirs = ["data"]
 
 project_dir = os.getenv("PROJECT_DIR", os.getcwd())
 if not project_dir:
     raise ValueError("PROJECT_DIR environment variable is not set")
 
+if env.IsCleanTarget():
+    for d in clean_dirs:
+        abs_path = join(project_dir,d)
+        if isdir(abs_path):
+            print("rmdir: ", abs_path)
+            shutil.rmtree(abs_path)
+    Return()
 
 web_src_dir = join(project_dir, "src", "http_server", "axe-os")
 dist_dir = join(web_src_dir, "dist", "axe-os")
@@ -27,7 +33,7 @@ if platform == "win32":
 if isdir(data_dir):
     rmtree(data_dir)
 
-subprocess.run([npm, "install"], cwd=web_src_dir, check=True)
+subprocess.run([npm, "install" ], cwd=web_src_dir, check=True)
 subprocess.run([npm, "run", "build"], cwd=web_src_dir, check=True)
 
 os.makedirs(data_dir)
